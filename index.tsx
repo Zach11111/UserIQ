946f01bb7afd6fcb13f4e34dc1776c80c8cad48d
zach orton <zacrut23@gmail.com>
2025-02-17 05:21:43 -0600

    initial commit

---
+++ index.tsx
@@ -0,0 +1,20 @@
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "UserIQ",
+    description: "A plugin that shows a users iq next to their name",
+    authors: [
+        {
+            id: 683550738198036516n,
+            name: "Zach Orange",
+        },
+        {
+            id: 871764313914871888n,
+            name: "CreeperITA104",
+        },
+    ],
+    patches: [],
+    // It might be likely you could delete these and go make patches above!
+    start() { },
+    stop() { },
+});
